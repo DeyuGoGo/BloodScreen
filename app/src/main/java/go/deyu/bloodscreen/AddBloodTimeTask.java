d0ce86c22fa129c49c8739949aa25a906bde6fe7
deyu <a0924xxx@gmail.com>
2015-05-13 16:44:57 +0800

    只有螢幕開著的時候會增加。

--- app/src/main/java/go/deyu/bloodscreen/AddBloodTimeTask.java
+++ app/src/main/java/go/deyu/bloodscreen/AddBloodTimeTask.java
@@ -2,6 +2,8 @@ package go.deyu.bloodscreen;
 
 import java.util.TimerTask;
 
+import go.deyu.bloodscreen.app.DeviceStatus;
+
 /**
  * Created by huangeyu on 15/5/4.
  */
@@ -9,8 +11,11 @@ public class AddBloodTimeTask extends TimerTask {
 
     private BloodControllerInterface mController;
 
+    private DeviceStatus mDeviceStatus ;
+
     public AddBloodTimeTask(BloodControllerInterface controller){
         this.mController = controller;
+        mDeviceStatus = DeviceStatus.getInstance();
     }
     /**
      * The task to run should be specified in the implementation of the {@code run()}
@@ -18,6 +23,6 @@ public class AddBloodTimeTask extends TimerTask {
      */
     @Override
     public void run() {
-        mController.addBlood();
+        if(mDeviceStatus.isScreenOn()) mController.addBlood();
     }
 }
